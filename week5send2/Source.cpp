c05e9da7c003ff37ec89d00882eeef1541c2e493
Supakorn Thongbor <69066050+frongtb@users.noreply.github.com>
2020-09-12 16:53:46 +0700

    Add project files.

---
+++ week5send2/Source.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <math.h>
+void calculatePow2(int);
+int main()
+{
+	int input_1;
+	scanf("%d", &input_1);
+	while (input_1 >= 0 || input_1 < 0)
+	{
+		if (input_1 <= 50)
+		{
+			calculatePow2(input_1);
+			return 0;
+		}
+		else
+			calculatePow2(input_1);
+		return 0;
+	}
+
+	return 0;
+}
+void calculatePow2(int input_1)
+{
+	int ans; -
+		printf("%.0f", pow(input_1, 2));
+}
